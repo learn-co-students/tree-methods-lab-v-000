125faf80cae204266de76ae8c213ed1a2efa665f
Alexandra <github email address>
2018-01-29 16:58:44 -0800

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function inOrder(currentNode){
+  if(currentNode.left){
+    inOrder(currentNode.left)
+  }
+  console.log(currentNode.data)
+  if(currentNode.right){
+    inOrder(currentNode.right)
+  }
+}
+
+function findOrAdd(){
+
+}
