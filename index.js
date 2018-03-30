210da2abd6d613a1a23aca994f22226bfe471465
github username <juliannne@gmail.com>
2018-03-29 21:52:19 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,47 @@
+function inOrder(currentNode) {
+  if(currentNode.left){
+    inOrder(currentNode.left)
+  }
+  console.log(currentNode.data)
+  if(currentNode.right){
+    inOrder(currentNode.right)
+  }
+}
+
+function findOrAdd(rootNode, newNode) {
+  let currentNode = rootNode;
+  if (rootNode.data == newNode.data) {
+    return true
+  }
+  if (newNode.data < rootNode.data) {
+    currentNode = rootNode.left
+    if (currentNode) {
+      return findOrAdd(currentNode, newNode)
+    } else {
+      return rootNode.left = newNode
+    }
+  } else if (newNode.data > rootNode.data) {
+    currentNode = rootNode.right
+    if (currentNode) {
+      return findOrAdd(currentNode, newNode)
+    } else {
+      return rootNode.right = newNode
+    }
+  }
+}
+
+function max(node) {
+  if (node.right) {
+    return max(node.right)
+  } else {
+    return node
+  }
+}
+
+function min(node) {
+  if (node.left) {
+    return min(node.left)
+  } else {
+    return node
+  }
+}
