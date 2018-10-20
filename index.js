057b863bcfff50ad11b245ded7cdedd7cab6e7e1
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-10-20 19:51:49 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,46 @@
+function inOrder(currentNode) {
+  if (currentNode.left) {
+    inOrder(currentNode.left);
+  }
+  console.log(currentNode.data);
+  if (currentNode.right) {
+    inOrder(currentNode.right);
+  }
+}
+
+function findOrAdd(rootNode, newNode) {
+  let currentNode = rootNode;
+  if (newNode.data < currentNode.data) {
+    currentNode = rootNode.left;
+    if (currentNode) {
+      return findOrAdd(currentNode, newNode);
+    } else {
+      rootNode.left = newNode;
+    }
+  } else if (newNode.data > currentNode.data) {
+    currentNode = rootNode.right;
+    if (currentNode) {
+      return findOrAdd(currentNode, newNode);
+    } else {
+      rootNode.right = newNode;
+    }
+  } else if (newNode.data === currentNode.data) {
+    return true;
+  }
+}
+
+function max(node) {
+  if (node.right) {
+    return max(node.right);
+  } else {
+    return node;
+  }
+}
+
+function min(node) {
+  if (node.left) {
+    return min(node.left);
+  } else {
+    return node;
+  }
+}
