b0b80fa28fb41ebe0f2d62f5cc82106941931601
Cristina Lugo <cmlugoce@syr.edu>
2018-10-31 14:42:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,40 @@
+function inOrder(currentNode){
+  if(currentNode.left){
+    inOrder(currentNode.left)
+  }
+  console.log(currentNode.data)
+  if(currentNode.right){
+    inOrder(currentNode.right)
+  }
+}
+ function findOrAdd(rootNode,newNode){
+  if(rootNode.data == newNode.data){
+    return true;
+  } else if(newNode.data < rootNode.data){
+    if(rootNode.left){
+      return findOrAdd(rootNode.left, newNode)
+    } else {
+      return rootNode.left = newNode;
+    }
+  } else if (newNode.data > rootNode.data){
+    if(rootNode.right){
+      return findOrAdd(rootNode.right, newNode)
+    } else {
+      return rootNode.right = newNode;
+    }
+  }
+}
+ function max(node){
+  if (node.right){
+    return max(node.right);
+  } else {
+    return node;
+  }
+}
+ function min(node){
+  if (node.left){
+    return min(node.left);
+  } else {
+    return node;
+  }
+}
