3f46267ed4ba0af36d0679b13c38a049ca02711a
Veronica Bustabad <vbustabad@gmail.com>
2019-01-27 01:41:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
 function inOrder(currentNode){
   if(currentNode.left){
-    inOrder(currentNode.left)
+    inOrder(currentNode.left);
   }
-  console.log(currentNode.data)
+  console.log(currentNode.data);
   if(currentNode.right){
-    inOrder(currentNode.right)
+    inOrder(currentNode.right);
+  }
+}
+
+function findOrAdd(rootNode, newNode){
+  let currentNode = rootNode;
+  if(newNode.data < rootNode.data){
+      currentNode = rootNode.left;
+      if(currentNode){
+        return findOrAdd(currentNode, newNode);
+      } else {
+        return rootNode.left = newNode;
+      }
+  } else if(newNode.data > rootNode.data) {
+    currentNode = rootNode.right;
+    if(currentNode){
+      return findOrAdd(currentNode, newNode);
+    } else {
+      return rootNode.right = newNode;
+    }
+  } else {
+    return true;
+  }
+}
+
+function max(node) {
+  if (node.right !== null) {
+    return max(node.right);
+  } else {
+    return node;
+  }
+}
+
+function min(node) {
+  if (node.left !== null) {
+    return min(node.left);
+  } else {
+    return node;
   }
 }
