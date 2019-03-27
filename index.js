adc22223956a2e31f15cb3d765d937545df05ca2
sgibson47 <sgibson47@gmail.com>
2019-03-27 13:08:38 -0500

    add findOrAdd() that adds new node to a bianary search tree

---
+++ index.js
@@ -0,0 +1,28 @@
+function inOrder(currentNode){
+  if(currentNode.left){
+    inOrder(currentNode.left)
+  }
+  console.log(currentNode.data)
+  if(currentNode.right){
+    inOrder(currentNode.right)
+  }
+}
+
+function findOrAdd(rootNode, newNode){
+  let currentNode = rootNode;
+  if(newNode.data < rootNode.data){
+    currentNode = rootNode.left;
+    if(currentNode){
+      findOrAdd(currentNode, newNode);
+    }else{
+      rootNode.left = newNode
+    }
+  }else if (newNode.data > rootNode.data){
+    currentNode = rootNode.right;
+    if(currentNode){
+      findOrAdd(currentNode, newNode);
+    }else{
+      rootNode.right = newNode
+    }
+  }
+}
