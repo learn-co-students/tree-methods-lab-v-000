770e94c408b2bb8221712ee6a659687ebe82f248
Paris Dolfman <parisdolfman@gmail.com>
2019-04-13 18:29:48 +0000

    Done.

--- index.js
+++ index.js
@@ -27,3 +27,19 @@ function findOrAdd(currentNode, newNode) {
     }
   }
 }
+
+function max(currentNode) {
+  if (currentNode.right) {
+    return max(currentNode.right)
+  } else {
+    return currentNode
+  }
+}
+
+function min(currentNode) {
+  if (currentNode.left) {
+    return min(currentNode.left)
+  } else {
+    return currentNode
+  }
+}
