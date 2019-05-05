0fa81f176f8bbf78b0805006dacd32545e02c908
rcrdlbl <bluh@outlook.com>
2019-05-04 20:28:35 -0400

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ function inOrder(currentNode) {
     inOrder(currentNode.right)
   }
 }
+
+function findOrAdd(rootNode, newNode) {
+  let currentNode = rootNode
+  if (newNode.data < rootNode.data) {
+    currentNode = rootNode.left
+    if (currentNode) {
+      findOrAdd(currentNode, newNode)
+    } else {
+      rootNode.left = newNode
+    }
+  } else if (newNode.data > rootNode.data) {
+    currentNode = rootNode.right
+    if (currentNode) {
+      findOrAdd(currentNode, newNode)
+    } else {
+      rootNode.right = newNode
+    }
+  }
+  return true
+}
