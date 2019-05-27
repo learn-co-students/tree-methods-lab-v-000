51486d344b58b77b81d6dc9dcbf2d21c01bbf2a2
Wali Rahman <wali.rahman89@yahoo.com>
2019-05-27 22:29:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
-function findOrAdd(rootNode, newNode){
-  let currentNode = rootNode.data
-  if(newNode.data < rootNode.data){
-      currentNode = rootNode.left
-      if(currentNode){
-        // do something
-      } else {
-        currentNode.left = newNode
-      }
-  } else if(newNode.data > rootNode.data) {
-    currentNode = rootNode.right
-    if(currentNode){
-      // do something
+function max(currentNode){
+  if(currentNode.right){
+    return max(currentNode.right)
+  } else {
+    return currentNode;
+  }
+}
+
+function min(currentNode){
+  if(currentNode.left){
+    return min(currentNode.left)
+  } else {
+    return currentNode;
+  }
+}
+
+function inOrder(currentNode){
+  if(currentNode.left){
+    inOrder(currentNode.left)
+  }
+  console.log(currentNode.data)
+
+  if(currentNode.right){
+    inOrder(currentNode.right)
+  }
+}
+
+function findOrAdd(currentNode, newNode){
+  if(currentNode.data == newNode.data){
+    return true
+  }
+  if(newNode.data < currentNode.data){
+    if(currentNode.left){
+      return findOrAdd(currentNode.left, newNode)
     } else {
-      currentNode.right = newNode
+      return currentNode.left = newNode
+    }
+  }
+
+  if(newNode.data > currentNode.data){
+    if(currentNode.right){
+      return findOrAdd(currentNode.right, newNode)
+    } else {
+      return currentNode.right = newNode
     }
   }
 }
