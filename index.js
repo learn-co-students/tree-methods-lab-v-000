4340dccc008de148956e4c9295e7dc5c1e5699e9
christianavdm <christianavdm@gmail.com>
2019-09-11 15:53:28 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function inOrder(currentNode){
+    if(currentNode.left){
+      inOrder(currentNode.left)
+    }
+    console.log(currentNode.data)
+    if(currentNode.right){
+      inOrder(currentNode.right)
+    }
+  }
+
+  function findOrAdd(rootNode, newNode){
+    let currentNode = rootNode; 
+    if(newNode.data == rootNode.data){
+        return true; 
+    } else if(newNode.data < rootNode.data){
+        currentNode = rootNode.left
+        if(currentNode){
+          findOrAdd(currentNode, newNode)
+        } else {
+          rootNode.left = newNode
+        }
+    } else if(newNode.data > rootNode.data) {
+      currentNode = rootNode.right
+      if(currentNode){
+        findOrAdd(currentNode, newNode)
+      } else {
+        rootNode.right = newNode
+      }
+    }
+    return true; 
+  }
